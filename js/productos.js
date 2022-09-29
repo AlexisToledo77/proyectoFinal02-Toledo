d705414723162c22a5838037e3e08bdf7998142b
Alexis Toledo <alexis.tole@gmail.com>
2022-09-29 18:21:22 -0300

    actualizacion desafio incorporar eventos

---
+++ js/productos.js
@@ -0,0 +1,105 @@
+/* const { divide } = require("lodash") */
+
+const todosLosProductos = document.getElementById("todosLosProductos")
+const botonCarrito = document.getElementById("botonCarrito")
+
+let productos = [
+    {
+        id: 1,
+        nombre: "Neón Abecedario",
+        detalle: "COLORES A ELECCION 50x40 cm.",
+        precio: 19999,
+        imagen: "https://i.ibb.co/2kkZHNp/neonAbc.png",
+    },
+    {
+        id: 2,
+        nombre: "Neón Yeah",
+        detalle: "COLORES A ELECCION 50x40 cm.",
+        precio: 21999,
+        imagen: "https://i.ibb.co/m0dQ2h9/neonYeah.png",
+    },
+    {
+        id: 3,
+        nombre: "Neón Smile",
+        detalle: "COLORES A ELECCION 50x40 cm.",
+        precio: 21999,
+        imagen: "https://i.ibb.co/fMbynFB/neon-Smile.png",
+    },
+    {
+        id: 4,
+        nombre: "Neón Flechas",
+        detalle: "COLORES A ELECCION 50x40 cm.",
+        precio: 23999,
+        imagen: "https://i.ibb.co/LYDv35M/neon-Flechas.png",
+    },
+    {
+        id: 5,
+        nombre: "Neón Girl Power",
+        detalle: "COLORES A ELECCION 50x40 cm.",
+        precio: 21999,
+        imagen: "https://i.ibb.co/BNdCJrX/neon-Girl-Power.png",
+    },
+    {
+        id: 6,
+        nombre: "Neón Cumpleaños",
+        detalle: "COLORES A ELECCION 50x40 cm.",
+        precio: 24999,
+        imagen: "https://i.ibb.co/7CyV2pz/neonHB.png",
+    },
+    {
+        id: 7,
+        nombre: "Neón Musica",
+        detalle: "COLORES A ELECCION 50x40 cm.",
+        precio: 20000,
+        imagen: "https://i.ibb.co/6P2sSDZ/neon-Musica.png",
+    },
+    {
+        id: 8,
+        nombre: "Neón Trago",
+        detalle: "COLORES A ELECCION 50x40 cm.",
+        precio: 20000,
+        imagen: "https://i.ibb.co/1LNmD6j/neon-Trago.png",
+    },
+
+]
+let carrito = []
+
+productos.forEach(producto => {
+    let productoRenderizado = document.createElement("todosLosProductos")
+    productoRenderizado.innerHTML = `
+                    <div class="m-3 card-img-top1" style="width: 19rem;">
+                        <img src="${producto.imagen}">
+                            <div class="card-body">
+                                <h1 class="card-title"> ${producto.nombre} </h1>
+                                <p class="card-text">  ${producto.detalle} </p>
+                                <p class="card-text">  $${producto.precio} </p>
+                                <button id=${producto.id}  class="botonComprar1">
+                                    <p> COMPRAR </p>
+                                </button>
+                            </div>
+                    </div>
+    `
+
+    todosLosProductos.append(productoRenderizado)
+    const botonCarrito = document.getElementById(producto.id)
+    botonCarrito.addEventListener("click", () => agregarCarrito(producto))
+    
+})
+
+const agregarCarrito = (producto) => {
+    let productoExiste = carrito.find(item => item.id === producto.id)
+    if (productoExiste !== undefined ){
+        productoExiste.precio = productoExiste.precio + producto.precio
+        productoExiste.cantidad = productoExiste.cantidad + 1 
+    } else{
+        carrito.push({
+            id: producto.id,
+            nombre:  producto.nombre,
+            precio:  producto.precio,
+            imagen: producto.imagen,
+            cantidad: 1
+        })
+    }
+}
+
+botonCarrito.addEventListener("click", () => console.log(carrito))
